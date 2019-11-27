54ee0695481b8cc9c9033625eefa64d81f73eec8
unknown <Administrator@MARKS-ASUS.redmond.corp.microsoft.com>
2019-11-27 12:35:49 -0800

    Moved header and footer to their own component's As well separated home welcome page to it's own component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { StaticHeaderComponent } from './static-header/static-header.component';
+import { StaticFooterComponent } from './static-footer/static-footer.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaticHeaderComponent,
+    StaticFooterComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
