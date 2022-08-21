be892461c37ebb45e6eebf2a4ea3436ccc94a3a0
Mark James Schemmer <mark.schemmer@isoftstone.com>
2022-08-20 22:10:00 -0700

    Added basic bouandry and movement of snake.

---
+++ src/app/common/utils.ts
@@ -0,0 +1,22 @@
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+
+// Common interface for keystroke directions
+export enum KeyStroke {
+    ArrowUp = "ArrowUp",
+    P = "P", 
+    S = "S",
+    ArrowLeft = "ArrowLeft",
+    ArrowRight = "ArrowRight",
+    R = "R"
+}
+
+export class Coordinate {
+    
+    public x: number; 
+    public y: number;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/app/snake/snake-board/snake.component.ts
+++ src/app/snake/snake-board/snake.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Coordinate, KeyStroke } from 'src/app/common/utils';
 
 @Component({
   selector: 'app-snake',
@@ -6,67 +7,209 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./snake.component.scss']
 })
 export class SnakeComponent implements OnInit {
-
-
   public ctx;
   public snakeBoard;
-
-  public snake;
-
+  public snake: Snake;
   private readonly RESOLUTION = 12;
-
   private readonly BOARD_DIMENSIONS = 800;
+  private readonly gameSpeed = 150;
+  public runner: Boolean = false;
+  public gameLooper = null;
+  public userInputForIteration: KeyStroke = null;
 
-  constructor() { }
+
+
+  constructor() {
+
+   }
 
   ngOnInit() {
     this.initialGameSetup();
   }
 
-  gameLoop = () => {
+  startGame = () => {
+    this.runner = true;
+    this.gameLooper = setInterval(this.gameLoop, this.gameSpeed)
+  }
 
+  stopGame = () => {
+    this.runner = false;
+    clearInterval(this.gameLooper);
+    this.gameLooper = null;
+  }
+
+  cleanBoard = () => {
+    this.ctx.clearRect(0, 0, 800, 800);
+  }
+
+  gameLoop = () => {
+    // game loop guard. 
+    if(this.runner === false || this.gameLooper === null) return;
+    // Take UserInput.
+    let userInput = this.userInputForIteration; 
+    // calculate
+      // taking user game input.
+    this.handleUserInput(userInput);
+    // moving game objects
+    // clean
+    // re-draw
+    this.draw();
+    // Reset user input to null so no uneeded commands are used on accident.
+    this.userInputForIteration = null;
+
+    this.startGame();
+  }
+
+  draw = () => {
+    // clean board
+    this.cleanBoard();
+    // draw snake
+    this.snake.drawSnake();
+  }
+
+  handleUserInput = (userInput: KeyStroke) => {
+    switch(userInput) {
+      case KeyStroke.S: {
+        if (this.gameLooper === null) {
+          console.log("game started.");
+          this.startGame();
+        }
+        break;
+      }
+      case KeyStroke.P: {
+        console.log("game stopped.");
+        this.stopGame();
+        break;
+      }
+      default: {
+        console.log("No valid user input for this iteration. ");
+      }
+    }
+  }
+
+  handleKeyboardStrokes = (key) => {
+    // use for starting and stoping game. 
+    // Snake Controls
+
+    console.log(key);
+    
+    const value: string = key.key.toLowerCase();
+
+    console.log(value);
+
+    switch(value) {
+      case KeyStroke.P.toLowerCase(): {
+        this.userInputForIteration = KeyStroke.P;
+        if (this.gameLooper != null) {
+          this.stopGame(); 
+          console.log(this.runner);
+        }
+        break;
+      }
+      case KeyStroke.S.toLowerCase(): {
+        this.userInputForIteration = KeyStroke.S;
+        if (this.gameLooper === null || this.runner === false) {
+          this.startGame();
+          console.log(this.runner);
+        }
+        break;
+      }
+      case KeyStroke.R.toLowerCase(): {
+        break;
+      }
+      case KeyStroke.ArrowLeft.toLowerCase(): {
+        break;
+      }
+      case KeyStroke.ArrowRight.toLowerCase(): {
+        break;
+      }
+      default: {
+        // log and forget.
+        // /console.log(value);
+      }
+    }
   }
 
   initialGameSetup = () => {
     // Get the board, and it's context.
     this.snakeBoard = document.getElementById("snake-board");
     this.ctx = this.snakeBoard.getContext("2d");
-
     this.snakeBoard.width = this.BOARD_DIMENSIONS;
     this.snakeBoard.height = this.BOARD_DIMENSIONS;
+    this.snake = new Snake(this.ctx, this.RESOLUTION, this.BOARD_DIMENSIONS);
 
-    this.snake = new Snake(this.ctx, this.RESOLUTION);
+    // attach event listener for keydown.
+    // window.addEventListener("keydown", this.handleKeyboardStrokes);
+    // window.addEventListener("keyup", this.handleKeyboardStrokes);
+
+    document.onkeydown = this.handleKeyboardStrokes;
+    document.onkeyup = this.handleKeyboardStrokes;
   }
 }
 
 class Snake {
   private ctx;
   private readonly black = "#000";
-
-  private readonly snakeBodyDimensions = 50;
-
+  private readonly snakeBodyDimensions = 50
   private readonly res;
+  private readonly boardDimensions
 
-  constructor(ctx, RESOLUTION){
+  private snakeBody: Coordinate;
+
+  constructor(ctx, RESOLUTION, boardDimensions){
     this.ctx = ctx;
     this.res = RESOLUTION;
+    this.boardDimensions = boardDimensions;
+    this.snakeBody = new Coordinate(400, 400);
     this.drawSnakeStartingPos();
   }
 
+  moveSnake = () => {
+
+    if (this.snakeBody.x > this.boardDimensions) {
+      this.snakeBody.x = 0;
+    } else if(this.snakeBody.x < 0) {
+      this.snakeBody.x = this.boardDimensions;
+    }
+
+    if (this.snakeBody.y > this.boardDimensions) {
+      this.snakeBody.y = 0;
+    } else if (this.snakeBody.y < 0) {
+      this.snakeBody.y = this.boardDimensions;
+    }
+
+    /// this.snakeBody.x = this.snakeBody.x + 0.5;
+    this.snakeBody.y = this.snakeBody.y + 0.5;
+  }
+
   drawSnakeStartingPos = () => {
+    const {x, y} = this.snakeBody;
     this.ctx.beginPath();
-    this.ctx.rect(400, 400, 1 * this.res, 1 * this.res);
+    this.ctx.rect(x, y, 1 * this.res, 1 * this.res);
     this.ctx.fillStyle = this.black;
     this.ctx.fill();
     this.ctx.closePath();
   }
 
   drawSnake = () => {
-    
+    // Move snake then draw snake.
+    this.moveSnake();
+    this.drawSnakeStartingPos();
   }
 }
 
-/*
+/* *Notes for game TODOS.*
+
   - Create a GameLoop Interface 
+  - Create a common tool library
+
+
+
+  Movement of snake..
+
+  +x -> moving right
+  -x -> moving left
+  +y -> moving down
+  -y -> moving up
 
 */
